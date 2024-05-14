cc73252da5d309abac4d7f186708fc4dd4d91a4d
Danywolf78 <neron41@gmail.com>
2024-05-13 23:12:51 -0300

    [actualizacion]

--- 03-mousse-follower/src/App.jsx
+++ 03-mousse-follower/src/App.jsx
@@ -1,35 +1,75 @@
-import './App.css'
-import { useEffect, useState } from 'react'
-
-function App() {
-  const [enable, setEnable] =useState(false)
+import "./App.css";
+import { useEffect, useState } from "react";
+const FollowMouse =() => {
+  const [enable, setEnable] = useState(false);
+  const [position, setPosition]=useState({x:0, y:0})
+  
+  //pointer move
   useEffect(() => {
-    console.log('Efecto',{enable})
-      }, [enable])
+    console.log('Efecto', { enable });
+  
+    const handleMove = (event) => {
+  
+      const { clientX, clientY } = event
+       console.log(handleMove,{clientX,clientY});
+       setPosition({x:clientX, y:clientY})
+  
+    }
+
+    if (enable) {
+      window.addEventListener("pointermove", handleMove);
+    }
+    // cleanup
+    // cuando el componente se desmonta
+    return () => {
+      window.removeEventListener('pointermove',handleMove)
+    }
+  }, [enable])
+
+  // change body cursor
+  useEffect(() => {
+document.body.classList.toggle('no-cursor',enable)
+return () =>{
+  document.body.classList.remove('no-cursor')
+}  
+},[enable])
+  
 
 
   return (
-    <main>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: '#F44336',
-        //border: '1px solid #fff',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -25,
-        top: -25,
-        width: 50,
-        height: 50,
-        transform: `translate(0px,0px)`
-      }}
+    <>
+     <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#F44336",
+          border: '1px solid #fff',
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 30,
+          height: 30,
+          transform: `translate(${position.x}px,${position.y}px)`,
+        }}
       />
 
-     
-    <button onClick={() => setEnable(!enable)}>{enable ?'Desactivar' :'Activar'} seguir puntero</button>
+      <button onClick={() => setEnable(!enable)}>
+        {enable ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </>
+ 
+   
+  );
+}
+function App() {
+  return(
+   <main>
+     < FollowMouse />
     </main>
-    
+
   )
+  
 }
 
-export default App
+export default App;
