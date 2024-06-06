7f1b63d1020d219ee8dca0c7e6665109396621bb
Danywolf78 <neron41@gmail.com>
2024-06-05 22:39:38 -0300

    [nuevo Proyecto]

---
+++ 05-react-buscador-peliculas/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+   
+   <div>
+    <header>
+      <form action=" ">
+          <input placeholder='Avengers, Matrix, Fast,'></input> 
+          <button type= 'submit'>Buscar</button>
+      </form>
+    </header>
+
+      <main>
+        Aqui iran los resultados
+      </main>
+      
+   </div>
+)
+}
+
+
+export default App
